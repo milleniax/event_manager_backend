c7fc2ac7cae88a3b4c90fcb4d78644fb9c2c9ba4
milleniax <marsel.abdullin.00@mail.ru>
2020-07-12 21:31:47 +0300

    Fix db settings

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -1,6 +1,11 @@
 import os
 from _datetime import timedelta
 from celery.schedules import crontab
+import dj_database_url
+
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -71,16 +76,7 @@ WSGI_APPLICATION = 'event_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'event_db',
-        'USER': 'event_admin',
-        'PASSWORD': 'event_manager_1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+
 
 
 # Password validation
